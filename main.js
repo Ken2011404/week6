8e515b86de4a8775f1b71c69792f4ac6e6af8c42
ken2011404 <ken2011404@hotmail.com>
2025-08-21 23:16:12 +0800

    修改輪播

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+console.log("Hello world");
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 24,
+    },
+  },
+  autoplay: true,
+});
